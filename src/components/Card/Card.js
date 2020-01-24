c15243d8c53fcd8cc6b3f5a76f69157a284a501f
Milosz Gawel <mgawel@gmail.com>
2020-01-24 19:56:43 +0100

    finished fetch methods and improved design

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,32 +1,102 @@
 import React from 'react';
 import styled from 'styled-components';
-// import weatherIcons from 'assets';
-// const icons = [...weatherIcons];
+import day from 'assets/background/day.jpg';
+import night from 'assets/background/night.jpg';
 
 const Content = styled.div`
-  margin: 10px auto 20px auto;
+  margin: 20px auto 20px auto;
   width: 90%;
-  flex-grow: 1;
+  height: 90vh;
   border: 1px solid #e95312;
   border-radius: 1em;
-  background-color: #fff;
-  display: ${props => (props.active ? 'block' : 'none')};
+  background-image: url(${props => (props.isDayTime ? day : night)});
+  display: ${props => (props.active ? 'flex' : 'none')};
+  flex-direction: column;
+  background-size: cover;
+`;
+
+const Text = styled.p`
+  font-family: 'Montserrat', sans-serif;
+  width: 100%;
+  text-align: center;
+  font-size: 1.4em;
+  color: ${props => (props.isDayTime ? 'black' : 'white')};
+`;
+
+const Temperature = styled.p`
+  position: relative;
+  margin: 0 auto;
+  width: 3em;
+  height: 2em;
+  line-height: 2em;
+  border-radius: 1em;
+  background-color: white;
+  top: -1em;
+  text-align: center;
+`;
+
+const Img = styled.img`
+  width: 60%;
+  margin: 20px auto 0 auto;
+`;
+
+const Info = styled.div`
+  position: relative;
+  margin: auto 0 0 0;
+  bottom: 0;
+  width: 100%;
+  height: 40%;
+  background-color: rgba(255, 255, 255, 0.5);
 `;
 
 const time = new Date();
 
 const Card = ({ data }) => {
   return (
-    <Content active={data.requestedCity}>
-      <h1>{data.requestedCity}</h1>
-      <p>{time.toLocaleTimeString()}</p>
-      <img
-        src={`https://developer.accuweather.com/sites/default/files/${data.picture}-s.png`}
-        alt="obrazek"
-      />
-      <p>temperatura</p>
-      <p>bezchmurnie</p>
-      <p>ciśnienie</p>
+    <Content active={data.city} isDayTime={data.isDayTime}>
+      <Img src={`./images/weatherIcons/${data.picture}-s.png`} alt="obrazek" />
+
+      <Info>
+        <Temperature
+          isDayTime={data.isDayTime}
+          style={{
+            fontSize: '3em',
+          }}
+        >
+          {data.temperature}
+          <i
+            className="far fa-circle"
+            style={{
+              fontSize: '0.2em',
+              fontWeight: 'normal',
+              position: 'relative',
+              top: '-3em',
+              marginRight: '0.4em',
+            }}
+          ></i>
+          <i
+            style={{
+              fontSize: '0.9em',
+            }}
+            className="fas fa-thermometer-quarter"
+          ></i>
+        </Temperature>
+        <Text
+          style={{
+            textTransform: 'uppercase',
+            fontSize: '2em',
+            position: 'relative',
+            top: '-20px',
+          }}
+          isDayTime={data.isDayTime}
+        >
+          {data.city}
+        </Text>
+        <Text isDayTime={data.isDayTime}>{time.toLocaleTimeString()}</Text>
+        <Text isDayTime={data.isDayTime} style={{ marginTop: '20px', textTransform: 'uppercase' }}>
+          {data.text}
+        </Text>
+      </Info>
     </Content>
   );
 };
--- src/layouts/MainPage/MainPage.js
+++ src/layouts/MainPage/MainPage.js
@@ -8,7 +8,7 @@ const MainWindow = styled.div`
   flex-direction: column;
   padding-top: 20px;
   font-family: sans-serif;
-  height: 100%;
+  max-height: 100%;
 `;
 
 const Header = styled.h2`
@@ -18,20 +18,47 @@ const Header = styled.h2`
 
 class WeatherApp extends Component {
   state = {
-    requestedCity: '',
-    city: 'jeden',
-    date: '11.11.2020',
-    picture: '01',
+    locationKey: '',
+    city: '',
+    picture: '',
+    temperature: '',
+    text: '',
+    isDayTime: false,
   };
 
   fetchCity = city => {
     fetch(
-      `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=s9wGCHoGfpd6EPFPsGyniPfgHgRJh4l4&q=${city}`,
+      // `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=s9wGCHoGfpd6EPFPsGyniPfgHgRJh4l4&q=${city}`,
+      `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=	8NohnMk0lcfUPmDAdCiaKBIwZYSQ5t46&q=${city}`,
+    )
+      .then(response => response.json())
+
+      .then(data => {
+        console.log(data);
+        this.setState({
+          city: data[0].LocalizedName,
+          locationKey: data[0].Key,
+        });
+
+        this.handleRequestData(this.state.locationKey);
+      })
+      .catch(err => console.log(err));
+  };
+
+  handleRequestData = key => {
+    fetch(
+      // `http://dataservice.accuweather.com/currentconditions/v1/${key}?apikey=s9wGCHoGfpd6EPFPsGyniPfgHgRJh4l4`,
+      `http://dataservice.accuweather.com/currentconditions/v1/${key}?apikey=	8NohnMk0lcfUPmDAdCiaKBIwZYSQ5t46`,
     )
       .then(response => response.json())
       .then(data => {
-        console.log(data);
-        this.setState({ requestedCity: data[0].LocalizedName });
+        this.setState({
+          picture: data[0].WeatherIcon,
+          temperature: data[0].Temperature.Metric.Value,
+          text: data[0].WeatherText,
+          isDayTime: data[0].IsDayTime,
+        });
+        window.localStorage.setItem('weatherData', JSON.stringify(this.state));
       })
       .catch(err => console.log(err));
   };
@@ -41,12 +68,10 @@ class WeatherApp extends Component {
     this.fetchCity(data);
   };
 
-  handleRequestData = () => {
-    fetch()
-      .than()
-      .than()
-      .catch(err => console.log(err));
-  };
+  componentDidMount() {
+    const data = JSON.parse(window.localStorage.getItem('weatherData'));
+    this.fetchCity(data.city);
+  }
 
   render() {
     return (
